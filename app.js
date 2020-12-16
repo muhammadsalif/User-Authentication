316f9a5b83113011004716ec73e4e7d3ded00d40
Muhamamd Salif <muhammad.salif@yahoo.com>
2020-12-17 00:04:43 +0500

    Leaving workspace, everything pushed.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(useragent.express());
 app.use(bodyParser.json())
 
 let users = [
+    // { userName: "Muhammad Salif", password: 123456 }
     // { id: 2, userName: "ali", password: 123 },
 ]
 console.log("Users array", users)
@@ -22,75 +23,84 @@ let sessions = [
     // { id: 2, token: "sdfrwerfew54er2rfwerwrw", expire: 1607424594798 },
 ]
 
+
+
 app.post("/signup", (req, res) => {
     if (!req.body || !req.body.userName || !req.body.password) {
         res.send("Information is missing")
+        console.log("Information is missing")
         return;
     }
-    users.push({
-        id: Math.ceil(Math.random() * 100),
-        userName: req.body.userName,
-        password: req.body.password
-    })
-    res.send("Sign up successfully")
-    console.log("User Signup successfully :", req.body.userName)
+    Bcrypt.stringToHash(JSON.stringify(req.body.password))
+        .then(passwordHash => {
+            users.push({
+                id: Math.ceil(Math.random() * 100),
+                userName: req.body.userName,
+                password: passwordHash
+            })
+            res.send("SignUp Successfully")
+            console.log("User Signup successfully :", req.body.userName)
+        })
 })
 
 app.post("/login", (req, res) => {
 
     if (!req.body || !req.body.userName || !req.body.password) {
         res.send("Username or password is Missing")
+        return
     }
     let currentUser = users.filter((eachUser) => eachUser.userName === req.body.userName);
+    currentUser = currentUser[0]
+    console.log("Current user", currentUser)
 
     if (currentUser) {
-        let passwordVerified = false
-        // Verifying user given password to the hash of that password in the database
-        Bcrypt.varifyHash(req.body.password, currentUser.password)
-            .then(result => {
-                if (result) {
-                    console.log("matched");
-                    return passwordVerified = result;
+
+        // Bcrypt.stringToHash(req.body.password)
+        //     .then(passwordHash => {
+        //         console.log("hash: ", passwordHash);
+
+        Bcrypt.varifyHash(JSON.stringify(req.body.password), currentUser.password)
+            .then(passwordVerified => {
+                console.log('password verified ', passwordVerified)
+
+                if (passwordVerified) {
+
+                    let tokenData = {
+                        ip: req.socket.remoteAddress,
+                        browserName: req.useragent.browser
+                    };
+
+                    Bcrypt.stringToHash(JSON.stringify(tokenData))
+                        .then(token => {
+                            sessions.push({
+                                id: currentUser.id,
+                                token: token,
+                                expire: new Date().getTime() + 300
+                            })
+
+                            console.log("Users sessions", sessions)
+                            res.send("Login success, token:" + token)
+                        })
                 } else {
-                    console.log("not matched");
+                    res.send("Password in invalid")
+                    console.log("Password is invalid");
                 }
             }).catch(e => {
                 console.log("error: ", e)
             })
-
-        if (passwordVerified) {
-
-            // make token using ip, browser name, 
-            let tokenData = {
-                ip: req.socket.remoteAddress,
-                browserName: req.useragent.browser
-            };
-            console.log("Token is : ", tokenData)
-
-            Bcrypt.stringToHash(JSON.stringify(tokenData))
-                .then(token => {
-                    sessions.push({
-                        id: currentUser.id,
-                        token: token,
-                        expire: new Date().getTime() + 300
-                    })
-
-                    res.send("Login success, token:" + token)
-                })
-            console.log("Session is : ", sessions)
-
-        } else {
-            res.send("password didnt match")
-        }
+        // })
+    } else {
+        res.send("Invalid username or password || user not found")
     }
-    else {
-        res.send("User not found")
-    }
+
+
 })
 
-app.get("/profile/:token", (req, res, next) => {
 
+app.get("/profile/:token", (req, res) => {
     let session = sessions.filter((eachSession) => eachSession.token === req.params.token)
+    console.log("Sessions", sessions)
+    console.log("Current session", session)
 
     if (!req.params.token) {
         res.send("token is Missing")
@@ -99,11 +109,12 @@ app.get("/profile/:token", (req, res, next) => {
         res.status(401).send("Token expired")
         sessions = sessions.filter((eachSession) => eachSession.token !== req.params.token)
     }
-    Bcrypt.validateHash(JSON.stringify(req.params.token))
-        .then(isValidHash => {
-            if (isValidHash) {
+    Bcrypt.validateHash(req.params.token)
+        .then(isValidTokenHash => {
+            console.log(isValidTokenHash)
+            if (isValidTokenHash) {
                 console.log("hash is valid")
-                Bcrypt.varifyHash(isValidHash, session.token)
+                Bcrypt.varifyHash(isValidTokenHash, session.token)
                     .then(hashVerified => {
                         if (hashVerified) {
 
@@ -116,6 +127,7 @@ app.get("/profile/:token", (req, res, next) => {
                         console.log("error: ", e)
                     })
             } else {
+                res.send("Not valid token")
                 console.log("hash is invalid")
             }
         })
